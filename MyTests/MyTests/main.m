f567111231671f390ccf1650f396794b49376e69
codwam <codwam@gmail.com>
2020-07-02 13:51:43 +0800

    MyTests

---
+++ MyTests/MyTests/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  MyTests
+//
+//  Created by Admin on 2020/7/2.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Test.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+//        NSLog(@"Hello, World!");
+        
+        [Test test];
+    }
+    return 0;
+}
